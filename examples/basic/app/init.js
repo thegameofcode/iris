b3c0843de97877af4681f6ecf89d42d4b5cc83d5
angelsanchez <angel.sanchez@intelygenz.com>
2013-02-07 13:32:06 +0100

    Updated examples

--- examples/basic/app/init.js
+++ examples/basic/app/init.js
@@ -1,28 +1,28 @@
 iris.path = {
-    welcome : "screen/welcome.js",
-    welcome_tmpl : "screen/welcome.html",
-    example_destroy : "screen/example_destroy.js",
-    example_destroy_tmpl : "screen/example_destroy.html",
-    example_destroy_all : "screen/example_destroy_all.js",
-    example_destroy_all_tmpl : "screen/example_destroy_all.html",
-    example_instance : "screen/example_instance.js",
-    example_instance_tmpl : "screen/example_instance.html",
-    example_list : "screen/example_list.js",
-    example_list_tmpl : "screen/example_list.html",
-    example_nested_uis : "screen/example_nested_uis.js",
-    example_nested_uis_tmpl : "screen/example_nested_uis.html",
-    example_screen_params : "screen/example_screen_params.js",
-    example_screen_params_tmpl : "screen/example_screen_params.html",
-    example_screen_params_child : "screen/example_screen_params_child.js",
-    example_screen_params_child_tmpl : "screen/example_screen_params_child.html",
-    example_template_params : "screen/example_template_params.js",
-    example_template_params_tmpl : "screen/example_template_params.html",
-    ui_example : "ui/example.js",
-    ui_example_tmpl : "ui/example.html",
-    ui_example_basic : "ui/example_basic.js",
-    ui_example_basic_tmpl : "ui/example_basic.html",
-    ui_nested : "ui/nested.js",
-    ui_nested_tmpl : "ui/nested.html"
+    welcome : "app/screen/welcome.js",
+    welcome_tmpl : "app/screen/welcome.html",
+    example_destroy : "app/screen/example_destroy.js",
+    example_destroy_tmpl : "app/screen/example_destroy.html",
+    example_destroy_all : "app/screen/example_destroy_all.js",
+    example_destroy_all_tmpl : "app/screen/example_destroy_all.html",
+    example_instance : "app/screen/example_instance.js",
+    example_instance_tmpl : "app/screen/example_instance.html",
+    example_list : "app/screen/example_list.js",
+    example_list_tmpl : "app/screen/example_list.html",
+    example_nested_uis : "app/screen/example_nested_uis.js",
+    example_nested_uis_tmpl : "app/screen/example_nested_uis.html",
+    example_screen_params : "app/screen/example_screen_params.js",
+    example_screen_params_tmpl : "app/screen/example_screen_params.html",
+    example_screen_params_child : "app/screen/example_screen_params_child.js",
+    example_screen_params_child_tmpl : "app/screen/example_screen_params_child.html",
+    example_template_params : "app/screen/example_template_params.js",
+    example_template_params_tmpl : "app/screen/example_template_params.html",
+    ui_example : "app/ui/example.js",
+    ui_example_tmpl : "app/ui/example.html",
+    ui_example_basic : "app/ui/example_basic.js",
+    ui_example_basic_tmpl : "app/ui/example_basic.html",
+    ui_nested : "app/ui/nested.js",
+    ui_nested_tmpl : "app/ui/nested.html"
 };
 
 
@@ -30,6 +30,7 @@ $(document).ready(
     function () {
 
         iris.translations("es-ES", "lang/es-es.js");
+        
         
         iris.locale(
             "es-ES", {
