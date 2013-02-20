fce15cfea0a05f2478f29ca9249c31021b69e4ec
surtich <surtich@gmail.com>
2013-02-20 23:37:11 +0100

    Improved iris_packager.js

--- tools/iris_packager.js
+++ tools/iris_packager.js
@@ -9,140 +9,164 @@ var initJs;
 var pathBase = "";
 
 function init () {
-	var inputPath, outputPath;
+ var inputPaths, outputPath;
 
-	process.argv.forEach(
-		function (val, index, array) {
-			var param;
+ process.argv.forEach(
+  function (val, index, array) {
+   var param;
 
-			param = getParam("input=", val);
-			if ( param !== null ) {
-				inputPath = param;
-			}
+   param = getParam("input=", val);
+   if ( param !== null ) {
+    inputPaths = param.split(",");
+   }
 
-			param = getParam("output=", val);
-			if ( param !== null ) {
-				outputPath = param;
-			}
+   param = getParam("output=", val);
+   if ( param !== null ) {
+    outputPath = param;
+   }
 
-			param = getParam("init=", val);
-			if ( param !== null ) {
-				initJs = param;
-			}
+   param = getParam("init=", val);
+   if ( param !== null ) {
+    initJs = param;
+   }
 
-			param = getParam("base=", val);
-			if ( param !== null ) {
-				pathBase = param;
-			}
-		}
-	);
+   param = getParam("base=", val);
+   if ( param !== null ) {
+    pathBase = param;
+   }
+  }
+  );
 
+ if ( !inputPaths ) {
+  throw "you must specify the parameter input=path_to_directory/,path_to_file...";
+ }
+ 
+ if ( !outputPath ) {
+  throw "you must specify the parameter output=path/";
+ }
 
-	if ( !inputPath ) {
-		throw "you must specify the parameter input=path/";
-	} else {
-		inputPath = validatePath(inputPath);
-	}
+ if ( !initJs ) {
+  throw "you must specify the parameter init=path/file.js";
+ }
 
-	if ( !outputPath ) {
-		throw "you must specify the parameter output=path/";
-	} else {
-		outputPath = validatePath(outputPath);
-	}
-
-	if ( !initJs ) {
-		throw "you must specify the parameter init=path/file.js";
-	}
-
-	outputFile = outputPath + initJs.match(/[^/]+\.js$/);
-
-	js.push(initJs);
-
-	var initPath = pathBase + inputPath;
-	console.log("scanning '" + initPath + "'...");
-	scanPath(initPath);
-	generateOutput();
+ if (fs.existsSync(outputPath) && fs.statSync(outputPath).isDirectory()) {
+  outputFile = validatePath(outputPath) + initJs.match(/[^/]+\.js$/);
+ } else {
+  outputFile = outputPath;
+ }
+ 
+ js.push(initJs);
+ 
+ var numInputPathsProccessed = 0
+ for (var i = 0; i < inputPaths.length; i++) {
+  var initPath = pathBase + inputPaths[i];
+  console.log("scanning '" + initPath + "'...");
+  (function(path) {
+   fs.stat(path, function(err, stats) {
+    if (err) throw err;
+    if (stats.isFile()) {
+     scanFile(path);
+    } else {
+     scanPath(validatePath(path));
+    }
+    numInputPathsProccessed++;
+    if (numInputPathsProccessed === inputPaths.length) {
+     generateOutput();
+    }
+   }); 
+  })(initPath);
+ }
+ 
+ 
 }
 
 function validatePath (path) {
-	if ( path.substr(-1) !== "/" ) {
-		path = path + "/";
-	}
-	return path;
+ if ( path.substr(-1) !== "/" ) {
+  path = path + "/";
+ }
+ return path;
 }
 
 function getParam (label, value) {
-	var idx = value.indexOf(label);
-	if ( idx > -1 ) {
-		return value.substring( idx + label.length );
-	}
-	return null;
+ var idx = value.indexOf(label);
+ if ( idx > -1 ) {
+  return value.substring( idx + label.length );
+ }
+ return null;
 }
 
 function scanPath (path) {
-	var filenames = fs.readdirSync(path);
-	var fullPath;
-	filenames.forEach(function(file) {
-		fullPath = path + file;
+ var filenames = fs.readdirSync(path);
+ var fullPath;
+ filenames.forEach(function(file) {
+  fullPath = path + file;
 
-	 	if ( file.substr(-5) === '.html' ) {
-			console.log("found js '" + fullPath + "'...");
-	 		var content = fs.readFileSync(fullPath, "utf8").replace(/[\r\n\t]/g,"");
-	 		html.push({path : path + file, content : content});
-
-	 	} else if ( file.substr(-3) === '.js' ) {
-			console.log("found html '" + fullPath + "'...");
-	 		js.push(fullPath)
-
-	 	} else if ( fullPath.indexOf(".") === -1 ) {
-			console.log("found dir '" + fullPath + "'...");
-	 		scanPath(fullPath + "/");
-	 	}
-	});
+  if ( fs.statSync(fullPath).isDirectory() ) {
+   console.log("found dir '" + fullPath + "'...");
+   scanPath(fullPath + "/");
+  } else if ( fs.statSync(fullPath).isFile() ) {
+   scanFile(fullPath);
+  }
+ });
 }
 
+function scanFile(file) {
+ if ( file.substr(-5) === '.html' ) {
+  console.log("found html '" + file + "'...");
+  var content = fs.readFileSync(file, "utf8").replace(/[\r\n\t]/g,"");
+  html.push({
+   path : file, 
+   content : content
+  });
+
+ } else if ( file.substr(-3) === '.js' ) {
+  console.log("found js '" + file + "'...");
+  js.push(file)
+
+ }
+}
 
 function generateOutput () {
 
-	// Compress using Google Closure
-	new compressor.minify({
-	    type: 'gcc',
-	    fileIn: js,
-	    fileOut: outputFile,
-	    callback: function(err){
-	    	if ( err ) {
-	        	throw err;
-	    	} else {
-	    		concatTemplates();
-	    	}
-	    }
-	});
+ // Compress using Google Closure
+ new compressor.minify({
+  type: 'gcc',
+  fileIn: js,
+  fileOut: outputFile,
+  callback: function(err){
+   if ( err ) {
+    throw err;
+   } else {
+    concatTemplates();
+   }
+  }
+ });
 }
 
 function concatTemplates(){
-	var content = [];
-	for ( var f=0, F=html.length;f<F; f++ ) {
-		content.push(
-			"iris.tmpl('"
-			+ html[f].path.replace(pathBase, "")
-			+"','"
-			+ html[f].content.replace(/\'/g, "\\\'")
-			+ "');\n"
-		);
-	}
+ var content = [];
+ for ( var f=0, F=html.length;f<F; f++ ) {
+  content.push(
+   "iris.Tmpl('"
+   + html[f].path.replace(pathBase, "")
+   +"','"
+   + html[f].content.replace(/\'/g, "\\\'")
+   + "');\n"
+   );
+ }
 
-	fs.appendFile(
-		outputFile,
-		content.join(""),
-		function(err) {
-    		if(err) {
-	        	console.error(err);
-	    	} 
-	    	else {
-	    		console.log("the file was saved in " + outputFile);
-	    	}
-		}
-	);	
+ fs.appendFile(
+  outputFile,
+  content.join(""),
+  function(err) {
+   if(err) {
+    console.error(err);
+   } 
+   else {
+    console.log("the file was saved in " + outputFile);
+   }
+  }
+  );	
 }
 
 init();
