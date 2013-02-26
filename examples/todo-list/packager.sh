b3a2519b723da942af5354b92746f7e7e79506c6
angelsanchez <angel.sanchez@intelygenz.com>
2013-02-26 12:06:28 +0100

    Update todo-list packager example

--- examples/todo-list/packager.sh
+++ examples/todo-list/packager.sh
@@ -3,6 +3,7 @@ rm -Rf packed
 mkdir packed
 cp index.html packed
 mkdir packed/css
-cp css/base.css packed/css
+mkdir packed/app
+cp css/* packed/css
 
-node ../../tools/iris_packager.js input='app' output='packed' init='app/init.js'
+node ../../tools/iris_packager.js base='app/' input='' output='packed/app' init='app/init.js'
