e2b561a2ed144338eba9c8fb5414b1b61f1c9e6d
angelsanchez <angel.sanchez@intelygenz.com>
2013-04-04 20:02:58 +0200

    Test for issue #91

---
+++ test/component/ui_repeated_dataid.js
@@ -0,0 +1,10 @@
+iris.ui(function (self) {
+
+	self.create = function() {
+
+		self.tmplMode(self.APPEND);
+		self.tmpl("test/component/ui_repeated_dataid.html");
+		
+	};
+
+},iris.path.ui_repeated_dataid);
