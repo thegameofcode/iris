33c731ef5e357a0a9021eee408d2a1dff100b1de
angelsanchez <angel.sanchez@intelygenz.com>
2013-04-20 00:31:44 +0200

    Updated todo list: fixed small bugs and code refactoring

--- examples/todo-list/app/init.js
+++ examples/todo-list/app/init.js
@@ -1,3 +1,4 @@
+// define iris components paths
 iris.path = {
 	welcome : { js: "welcome.js", html: "welcome.html" },
     todo: { js: "todo.js", html : "todo.html" },
@@ -6,8 +7,12 @@ iris.path = {
 
 
 $(window.document).ready(
+
     function () {
+    	// set the iris components base uri
     	iris.baseUri("app/");
+
+    	// show the initial screen
         iris.welcome(iris.path.welcome.js);
     }
 );
