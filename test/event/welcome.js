b1f015c5c5c102bb5782a058a153b598f37c98b7
Angel Sanchez <angel.sanchez@intelygenz.com>
2013-12-18 17:54:32 +0100

    New files to test iris.Event

---
+++ test/event/welcome.js
@@ -0,0 +1,27 @@
+iris.screen(function (self) {
+
+	self.create = function() {
+		self.tmpl(iris.path.welcome_tmpl);
+
+		self.on("test-event", testFunc);
+		self.on("remove-test-event", removeTestFunc);
+		self.on("remove-all-test-event", removeAllTestFunc);
+	};
+
+	function testFunc () {
+		window.ok(true, 'Executed test-event callback');
+	}
+
+	function removeTestFunc () {
+		window.ok(true, 'Executed remove-test-event callback once');
+		self.off("remove-test-event", removeTestFunc);
+		self.notify("remove-test-event"); // No callbacks after self.off
+	}
+
+	function removeAllTestFunc () {
+		window.ok(true, 'Executed remove-all-test-event callback once');
+		self.off("remove-all-test-event");
+		self.notify("remove-all-test-event"); // No callbacks after self.off
+	}
+
+},iris.path.welcome);
