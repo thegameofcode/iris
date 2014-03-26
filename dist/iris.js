05deb3afef7a8300b03c82318291b39942189fa2
Angel Sanchez <angel.sanchez@intelygenz.com>
2014-03-26 18:00:46 +0100

    Code refactoring in iris.Resource

--- dist/iris.js
+++ dist/iris.js
@@ -2116,7 +2116,7 @@
         iris.Event.prototype
     );
 
-    Resource.prototype.ajax = function(p_method, p_path, p_params, f_success, f_error) {
+    Resource.prototype.ajax = function(p_method, p_path, p_params) {
 
         return iris.ajax({
             "url": this.setting("path") + p_path,
@@ -2125,32 +2125,26 @@
             "cache": false,
             "dataType": this.setting("type"),
             "async": true,
-            "success": f_success,
             "error": function (p_request, p_textStatus, p_errorThrown) {
-
                 iris.notify(iris.RESOURCE_ERROR, {request: p_request, status: p_textStatus, error: p_errorThrown});
-
-                if ( f_error !== undefined ) {
-                    f_error( p_request, p_textStatus, p_errorThrown );
-                }
             }
         });
     };
 
-    Resource.prototype.get = function(p_path, f_success, f_error) {
-        return this.ajax("GET", p_path, null, f_success, f_error);
+    Resource.prototype.get = function(p_path, p_params) {
+        return this.ajax("GET", p_path, p_params);
     };
 
-    Resource.prototype.del = function(p_path, f_success, f_error) {
-        return this.ajax("DELETE", p_path, null, f_success, f_error);
+    Resource.prototype.del = function(p_path) {
+        return this.ajax("DELETE", p_path, null);
     };
 
-    Resource.prototype.put = function(p_path, p_params, f_success, f_error) {
-        return this.ajax("PUT", p_path, p_params, f_success, f_error);
+    Resource.prototype.put = function(p_path, p_params) {
+        return this.ajax("PUT", p_path, p_params);
     };
 
-    Resource.prototype.post = function(p_path, p_params, f_success, f_error) {
-        return this.ajax("POST", p_path, p_params, f_success, f_error);
+    Resource.prototype.post = function(p_path, p_params) {
+        return this.ajax("POST", p_path, p_params);
     };
 
     //
--- test/resource/resource.js
+++ test/resource/resource.js
@@ -3,19 +3,19 @@ iris.resource(function(self){
     self.settings({path : "http://localhost:8081/"});
 
     self.load = function (id, success, error) {
-      self.get("test/resource/" + id, success, error);
+      self.get("test/resource/" + id).done(success).fail(error);
     };
 
     self.create = function (params, success, error) {
-      self.post("echo/create", params, success, error);
+      self.post("echo/create", params).done(success).fail(error);
     };
 
     self.update = function (id, params, success, error) {
-      self.put("echo/put/" + id, params, success, error);
+      self.put("echo/put/" + id, params).done(success).fail(error);
     };
 
     self.remove = function (id, success, error) {
-      self.del("echo/delete/" + id, success, error);
+      self.del("echo/delete/" + id).done(success).fail(error);
     };
 
 }, iris.path.resource);
