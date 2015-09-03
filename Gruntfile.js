89ce63101b0c976096e68de65801769fe40fe827
unlogic <unlogic@gmail.com>
2015-09-02 17:48:18 -0700

    Update Gruntfile.js

--- Gruntfile.js
+++ Gruntfile.js
@@ -96,6 +96,22 @@ module.exports = function(grunt) {
                 testname: "qunit tests from saucelabs"
             }
         }
+    },
+    coveralls: {
+      // Options relevant to all targets
+      options: {
+        // When true, grunt-coveralls will only print a warning rather than
+        // an error, to prevent CI builds from failing unnecessarily (e.g. if
+        // coveralls.io is down). Optional, defaults to false.
+        force: false
+      },
+      your_target: {
+        // LCOV coverage file (can be string, glob or array)
+        src: 'coverage-results/extra-results-*.info',
+        options: {
+          // Any options for just this target
+        }
+      }
     }
   });
 
